149153217cf736a9be349626b6aea3810fddd8be
roboto <roboto@gmail.com>
2021-02-23 14:35:48 -0600

    Fixing ReloadableApiErrorResourceBundle

--- clients-app/src/main/java/com/roboto/clients/application/contexts/ApplicationContext.java
+++ clients-app/src/main/java/com/roboto/clients/application/contexts/ApplicationContext.java
@@ -1,9 +1,7 @@
 package com.roboto.clients.application.contexts;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import com.roboto.clients.message.source.ReloadableApiErrorResourceBundle;
@@ -11,28 +9,28 @@ import com.roboto.clients.message.source.ReloadableApiErrorResourceBundle;
 @Configuration
 public class ApplicationContext {
 	
-	/*
-	 * @Bean public ReloadableApiErrorResourceBundle apiErrorMessageSource() {
-	 * ReloadableApiErrorResourceBundle apiErrorMessageSource = new
-	 * ReloadableApiErrorResourceBundle();
-	 * apiErrorMessageSource.setBasename("classpath:messages");
-	 * apiErrorMessageSource.setCacheSeconds(3600); return apiErrorMessageSource; }
-	 */
 	
-	@Bean
-	public MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-
-		messageSource.setBasename("classpath:messages");
-		messageSource.setDefaultEncoding("UTF-8");
-		messageSource.setCacheSeconds(3600);
-		return messageSource;
-	}
+	  @Bean public ReloadableApiErrorResourceBundle apiErrorMessageSource() {
+	  ReloadableApiErrorResourceBundle apiErrorMessageSource = new
+	  ReloadableApiErrorResourceBundle();
+	  apiErrorMessageSource.setBasename("classpath:messages");
+	  apiErrorMessageSource.setCacheSeconds(3600); return apiErrorMessageSource; }
+	 
+	
+	/*
+	 * @Bean public MessageSource messageSource() {
+	 * ReloadableResourceBundleMessageSource messageSource = new
+	 * ReloadableResourceBundleMessageSource();
+	 * 
+	 * messageSource.setBasename("classpath:messages");
+	 * messageSource.setDefaultEncoding("UTF-8");
+	 * messageSource.setCacheSeconds(3600); return messageSource; }
+	 */
 
 	@Bean
 	public LocalValidatorFactoryBean getValidator() {
 		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-		bean.setValidationMessageSource(messageSource());
+		bean.setValidationMessageSource(apiErrorMessageSource());
 		return bean;
 	}
 
